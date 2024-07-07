352a8cbba8a46f10661566c05ad11e609535cf0d
Pankajkaushik-del <kaushik.pankaj87@gmail.com>
2024-07-07 17:09:44 -0500

    Add files via upload

---
+++ GITproject.py
@@ -0,0 +1,9 @@
+light = input("light color:")
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("Yield")
+elif(light=="green"):
+    print("Go")
+else:
+    print("light is broken")
